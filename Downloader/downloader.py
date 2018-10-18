7ec43e7b3551d51e58aa6838418446e63441c176
Alain Igban <apigban@gmail.com>
2018-10-18 20:51:36 +0800

    Added download() function. Logging feature also added

--- Downloader/downloader.py
+++ Downloader/downloader.py
@@ -6,7 +6,7 @@ from time import time
 download_logger = log.get_logger(__name__)
 
 attachment_id_start = 2708
-attachment_id_end = 3738
+attachment_id_end = 3739
 
 attachment_id_list = list(range(attachment_id_start, attachment_id_end))
 
@@ -19,8 +19,19 @@ def uri_creator(base_uri, start, end, id_list):
     for attachment_id in attachment_id_list:
         uri_list.append(uri + str(attachment_id))
 
-    print(uri_list)
+    download_logger.info(f'URI List Generated for attachment ids: [{attachment_id_start},{attachment_id_end}]')
     return uri_list
 
 
-uri_creator(uri, attachment_id_start, attachment_id_end, attachment_id_list)
+def download(download_list):
+    for link in download_links:
+        download_logger.info(f'Downloading link {link}')
+        requests = requests.get(link)
+        with open(f'dump/{attachment_id}.csv', 'wb') as attachment:
+            attachment.write(requests.content)
+        download_logger.info(f'Link Downloaded.')
+
+
+download_links = uri_creator(uri, attachment_id_start, attachment_id_end, attachment_id_list)
+
+download(download_links)
