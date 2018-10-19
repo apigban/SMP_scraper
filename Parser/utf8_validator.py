f9a233dd3bfdabaac83ab5e89a945ab98d0e76ff
Alain Igban <apigban@gmail.com>
2018-10-19 16:42:03 +0800

    Simplified output to filename of files that passed the utf-8 encoding check

--- Parser/utf8_validator.py
+++ Parser/utf8_validator.py
@@ -17,21 +17,23 @@ def file_walker():
 def csv_validator(csv_file_list):
     """
     Checks if csv_file in csv_file_list can be opened using utf-8 encoding.
-    A file that passed the check will have a "VALID" tag written to invalid.csv
+    A file that passed the check will have a "VALID" tag written to invalid.txt
     A file that is encoded differently from utf-8 (did not pass) will have
     an "INVALID" tag.
     :param csv_file_list:
     """
     for csv_file in csv_file_list:
+
         try:
             file = codecs.open(f'{file_dump_path}/{csv_file}', encoding='utf-8', errors='strict')
             for line in file:
                 pass
-            with open(f'/home/apigban/PycharmProjects/SMP_scraper/Downloader/log/invalid.csv', 'a') as open_file:
-                open_file.write(f'{csv_file} has VALID utf-8\n')
+            with open(f'/home/apigban/PycharmProjects/SMP_scraper/Parser/log/valid.txt', 'a') as open_file:
+                open_file.write(f'{csv_file}\n')
+
         except UnicodeDecodeError:
-            with open(f'/home/apigban/PycharmProjects/SMP_scraper/Downloader/log/invalid.csv', 'a') as open_file:
-                open_file.write(f'{csv_file} has INVALID utf-8\n')
+            with open(f'/home/apigban/PycharmProjects/SMP_scraper/Parser/log/invalid.txt', 'a') as open_file:
+                open_file.write(f'{csv_file}\n')
 
 
 csv_list = file_walker()
