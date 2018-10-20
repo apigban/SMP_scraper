27fd3aef7e02506a2386adefd89519014ca4dac6
Alain Igban <apigban@gmail.com>
2018-10-20 13:04:54 +0800

    Added docstrings to functions and reformatted the code

--- Parser/file_cleaner.py
+++ Parser/file_cleaner.py
@@ -1,17 +1,16 @@
-import os
 import csv
-import Log.log as log
+import os
 import re
 
-
-# def file_walker():
-#   for root, dirs, files in os.walk(f'{data_path}'):
-#        for filename in files:
-#            file_list.append(filename)
-#    return file_list
+import Log.log as log
 
 
 def get_invalid_file():
+    """
+    Opens INvalid_file_log, converts the file to a csv.reader object and
+    reads all items from the object.
+    :return:
+    """
     invalid_files_list = []
 
     with open(invalid_file_log) as csv_file:
@@ -23,6 +22,11 @@ def get_invalid_file():
 
 
 def get_Valid_file():
+    """
+    Opens valid_file_log, converts the file to a csv.reader object and
+    reads all items from the object. Returns a valid_files_list.
+    :return:
+    """
     valid_files_list = []
 
     with open(valid_file_log) as csv_file:
@@ -34,6 +38,11 @@ def get_Valid_file():
 
 
 def delete_invalid_file(invalid_files_list):
+    """
+    Iterates through invalid_files_list to remove invalid files from data_path
+    If a file is not found, log the exception and continue iteration
+    :return: None
+    """
     for invalid_file in invalid_files_list:
         try:
             os.remove(f'{data_path}{invalid_file}')
@@ -63,21 +72,22 @@ def preproc_valid_files(files_for_preproc):
     return pre_processing_list
 
 
-# Initialize Logger
-file_cleaner_log = log.get_logger(__name__)
+if __name__ == '__main__':
+    # Initialize Logger
+    file_cleaner_log = log.get_logger(__name__)
 
-# Present Data Dump Path
-data_path = '/home/apigban/PycharmProjects/SMP_scraper/Downloader/dump/'
+    # Present Data Dump Path
+    data_path = '/home/apigban/PycharmProjects/SMP_scraper/Downloader/dump/'
 
-# Deletion Process for files that DIDN'T PASS the utf-8 encoding check
-invalid_file_log = '/home/apigban/PycharmProjects/SMP_scraper/Parser/log/invalid.txt'
-files_for_deletion = get_invalid_file()
-delete_invalid_file(files_for_deletion)
+    # Deletion Process for files that DIDN'T PASS the utf-8 encoding check
+    invalid_file_log = '/home/apigban/PycharmProjects/SMP_scraper/Parser/log/invalid.txt'
+    files_for_deletion = get_invalid_file()
+    delete_invalid_file(files_for_deletion)
 
-# Pre-process for files that PASSED the utf-8 encoding check
-valid_file_log = '/home/apigban/PycharmProjects/SMP_scraper/Parser/log/valid.txt'
-files_for_processsing = get_Valid_file()
+    # Pre-process for files that PASSED the utf-8 encoding check
+    valid_file_log = '/home/apigban/PycharmProjects/SMP_scraper/Parser/log/valid.txt'
+    files_for_processsing = get_Valid_file()
 
-string_pattern = "\A\^FINANCIAL"
+    string_pattern = "\A\^FINANCIAL"
 
-preproc_valid_files(files_for_processsing)
+    preproc_valid_files(files_for_processsing)
