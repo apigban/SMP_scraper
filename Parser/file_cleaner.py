c6a3ed0183b50e45f530b37e1da181a4debeb170
Alain Igban <apigban@gmail.com>
2018-10-20 14:03:18 +0800

    Added doctrings and removed hardcoded paths.

--- Parser/file_cleaner.py
+++ Parser/file_cleaner.py
@@ -33,7 +33,6 @@ def get_Valid_file():
         all_rows_obj = csv.reader(csv_file)
         for row in all_rows_obj:
             valid_files_list.append(row[0])
-
     return valid_files_list
 
 
@@ -52,14 +51,14 @@ def delete_invalid_file(invalid_files_list):
             pass
 
 
-def preproc_valid_files(files_for_preproc):
-    valid_file_counter = None
+def preproc_valid_files(files_for_processing, valid_file_log_length):
+    valid_file_counter = 0
     invalid_file_counter = None
 
-    pre_processing_list = []
+    pre_processed_list = []
 
-    for valid_csv_file in files_for_processsing:
-        with open(f'{data_path}{valid_csv_file}') as csv_file:
+    for valid_csv_file in files_for_processing:
+        with open(f'{data_path}/{valid_csv_file}') as csv_file:
             all_row_obj = next(csv.reader(csv_file))
 
             for row in all_row_obj:
@@ -68,8 +67,13 @@ def preproc_valid_files(files_for_preproc):
                     file_cleaner_log.info(f'{valid_csv_file} contains non-financial data')
                 else:
                     file_cleaner_log.info(f'{valid_csv_file} is a valid csv file')
-                    pre_processing_list.append(f'{valid_csv_file}')
-    return pre_processing_list
+                    valid_file_counter += 1
+                    pre_processed_list.append(f'{valid_csv_file}')
+    file_cleaner_log.info(
+        f'{valid_file_counter} pre-processed files. Files are checked if utf-8 encoded and not an HTTP response.')
+    file_cleaner_log.info(f'{valid_file_log_length - valid_file_counter} failed to pass pre-processing checks.')
+
+    return pre_processed_list
 
 
 if __name__ == '__main__':
@@ -77,17 +81,19 @@ if __name__ == '__main__':
     file_cleaner_log = log.get_logger(__name__)
 
     # Present Data Dump Path
-    data_path = '/home/apigban/PycharmProjects/SMP_scraper/Downloader/dump/'
+    data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Downloader/dump/'))
 
     # Deletion Process for files that DIDN'T PASS the utf-8 encoding check
-    invalid_file_log = '/home/apigban/PycharmProjects/SMP_scraper/Parser/log/invalid.txt'
+    invalid_file_log = os.path.dirname(__file__) + '/log/invalid.txt'
     files_for_deletion = get_invalid_file()
     delete_invalid_file(files_for_deletion)
 
     # Pre-process for files that PASSED the utf-8 encoding check
-    valid_file_log = '/home/apigban/PycharmProjects/SMP_scraper/Parser/log/valid.txt'
-    files_for_processsing = get_Valid_file()
-
+    # Pre-processing steps:
+    #   1. Load row
+    #   2. if pattern is not found:     log to file
+    #   3. if pattern is found:         append filename to pre_processed_list
+    #   4. return pre_processed_list
+    valid_file_log = os.path.dirname(__file__) + '/log/valid.txt'
     string_pattern = "\A\^FINANCIAL"
-
-    preproc_valid_files(files_for_processsing)
+    preproc_valid_files(get_Valid_file(), len(get_Valid_file()))
