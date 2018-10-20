ca988dfbb41c322a973929a036cbc6277cc16b29
Alain Igban <apigban@gmail.com>
2018-10-20 15:13:54 +0800

    Refactored code by removing implicit executions. file_cleaner.py and utf8_validator are now modules that is imported to the main program, pre_processor.py

--- Parser/file_cleaner.py
+++ Parser/file_cleaner.py
@@ -1,8 +1,28 @@
 import csv
 import os
 import re
-
 import Log.log as log
+
+### Validation steps:
+#   1. Load row
+#   2. if pattern is not found:     log to file
+#   3. if pattern is found:         append filename to pre_processed_list
+#   4. return pre_processed_list
+
+
+# Initialize Logger
+file_cleaner_log = log.get_logger(__name__)
+
+# Present Data Dump Path
+data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Downloader/dump/'))
+
+# Deletion Process for files that DIDN'T PASS the utf-8 encoding check
+invalid_file_log = os.path.dirname(__file__) + '/log/invalid.txt'
+# files_for_deletion = get_invalid_file()
+# delete_invalid_file(files_for_deletion)
+
+valid_file_log = os.path.dirname(__file__) + '/log/valid.txt'
+string_pattern = "\A\^FINANCIAL"
 
 
 def get_invalid_file():
@@ -70,7 +90,7 @@ def preproc_valid_files(files_for_processing, valid_file_log_length):
                     valid_file_counter += 1
                     pre_processed_list.append(f'{valid_csv_file}')
     file_cleaner_log.info(
-        f'{valid_file_counter} pre-processed files. Files are checked if utf-8 encoded and not an HTTP response.')
+        f'{valid_file_counter} pre-processed files. Files are checked if utf-8 encoded and starts with ^FINANCIAL.')
     file_cleaner_log.info(f'{valid_file_log_length - valid_file_counter} failed to pass pre-processing checks.')
 
     return pre_processed_list
@@ -88,12 +108,9 @@ if __name__ == '__main__':
     files_for_deletion = get_invalid_file()
     delete_invalid_file(files_for_deletion)
 
-    # Pre-process for files that PASSED the utf-8 encoding check
-    # Pre-processing steps:
-    #   1. Load row
-    #   2. if pattern is not found:     log to file
-    #   3. if pattern is found:         append filename to pre_processed_list
-    #   4. return pre_processed_list
+    # Validation for files that PASSED the utf-8 encoding check
     valid_file_log = os.path.dirname(__file__) + '/log/valid.txt'
     string_pattern = "\A\^FINANCIAL"
     preproc_valid_files(get_Valid_file(), len(get_Valid_file()))
+
+    #print(all_files_from_dump)
--- Parser/utf8_validator.py
+++ Parser/utf8_validator.py
@@ -2,19 +2,18 @@
 import os
 import codecs
 
+file_dump_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Downloader/dump/'))
 file_list = []
 
-file_dump_path = '/home/apigban/PycharmProjects/SMP_scraper/Downloader/dump'
 
-
-def file_walker():
-    for root, dirs, files in os.walk('/home/apigban/PycharmProjects/SMP_scraper/Downloader/dump'):
+def file_walker(dump_path):
+    for root, dirs, files in os.walk(file_dump_path):
         for filename in files:
             file_list.append(filename)
     return file_list
 
 
-def csv_validator(csv_file_list):
+def csv_validator():
     """
     Checks if csv_file in csv_file_list can be opened using utf-8 encoding.
     A file that passed the check will have a "VALID" tag written to invalid.txt
@@ -22,19 +21,26 @@ def csv_validator(csv_file_list):
     an "INVALID" tag.
     :param csv_file_list:
     """
-    for csv_file in csv_file_list:
 
+    csv_file_list = file_walker(file_dump_path)
+
+    valid_file_list = []
+    invalid_file_list = []
+    for csv_file in csv_file_list:
         try:
             file = codecs.open(f'{file_dump_path}/{csv_file}', encoding='utf-8', errors='strict')
             for line in file:
                 pass
-            with open(f'/home/apigban/PycharmProjects/SMP_scraper/Parser/log/valid.txt', 'a') as open_file:
-                open_file.write(f'{csv_file}\n')
-
+            # with open(f'/home/apigban/PycharmProjects/SMP_scraper/Parser/log/valid.txt', 'a') as open_file:
+            #    open_file.write(f'{csv_file}\n')
+            valid_file_list.append(csv_file)
         except UnicodeDecodeError:
-            with open(f'/home/apigban/PycharmProjects/SMP_scraper/Parser/log/invalid.txt', 'a') as open_file:
-                open_file.write(f'{csv_file}\n')
+            # with open(f'/home/apigban/PycharmProjects/SMP_scraper/Parser/log/invalid.txt', 'a') as open_file:
+            #    open_file.write(f'{csv_file}\n')
+            invalid_file_list.append(csv_file)
+
+    return valid_file_list, invalid_file_list
 
 
-csv_list = file_walker()
-csv_validator(csv_list)
+if __name__ == '__main__':
+    csv_validator()
