a75f7b20ff0bbc165fa2501e609ed8a79b0ca31f
Alain Igban <apigban@gmail.com>
2018-10-20 12:53:59 +0800

    Added logging feature, log file path is Parser/log/
    Added function get_Validd_file(), which opens valid_file_log, converts the file to a csv.reader object and reads all items from the object.
    
    Added Exception handling to delete_invalid_file(), if File is not found, error is logged and iteration continues.
    
    Added inline comments

--- Parser/file_cleaner.py
+++ Parser/file_cleaner.py
@@ -1,38 +1,83 @@
 import os
 import csv
 import Log.log as log
-
-file_cleaner_log = log.get_logger(__name__)
-
-data_path = '/home/apigban/PycharmProjects/SMP_scraper/Downloader/dump/'
-
-target_file = '/home/apigban/PycharmProjects/SMP_scraper/Parser/log/invalid.txt'
+import re
 
 
-def file_walker():
-    for root, dirs, files in os.walk('/home/apigban/PycharmProjects/SMP_scraper/Downloader/dump'):
-        for filename in files:
-            file_list.append(filename)
-    return file_list
+# def file_walker():
+#   for root, dirs, files in os.walk(f'{data_path}'):
+#        for filename in files:
+#            file_list.append(filename)
+#    return file_list
 
 
 def get_invalid_file():
-    invalid_files = []
+    invalid_files_list = []
 
-    with open(target_file) as csv_file:
+    with open(invalid_file_log) as csv_file:
         all_rows_obj = csv.reader(csv_file)
         for row in all_rows_obj:
-            invalid_files.append(row[0])
+            invalid_files_list.append(row[0])
 
-    return invalid_files
+    return invalid_files_list
+
+
+def get_Valid_file():
+    valid_files_list = []
+
+    with open(valid_file_log) as csv_file:
+        all_rows_obj = csv.reader(csv_file)
+        for row in all_rows_obj:
+            valid_files_list.append(row[0])
+
+    return valid_files_list
 
 
 def delete_invalid_file(invalid_files_list):
     for invalid_file in invalid_files_list:
-        os.remove(f'{data_path}{invalid_file}')
-        file_cleaner_log.info(f'{invalid_file} removed')
+        try:
+            os.remove(f'{data_path}{invalid_file}')
+            file_cleaner_log.info(f'{invalid_file} removed')
+        except FileNotFoundError as FNFError:
+            file_cleaner_log.error(f'{invalid_file} not found. Error: {FNFError}')
+            pass
 
 
+def preproc_valid_files(files_for_preproc):
+    valid_file_counter = None
+    invalid_file_counter = None
+
+    pre_processing_list = []
+
+    for valid_csv_file in files_for_processsing:
+        with open(f'{data_path}{valid_csv_file}') as csv_file:
+            all_row_obj = next(csv.reader(csv_file))
+
+            for row in all_row_obj:
+                match = re.match(string_pattern, str(row))
+                if match is None:
+                    file_cleaner_log.info(f'{valid_csv_file} contains non-financial data')
+                else:
+                    file_cleaner_log.info(f'{valid_csv_file} is a valid csv file')
+                    pre_processing_list.append(f'{valid_csv_file}')
+    return pre_processing_list
+
+
+# Initialize Logger
+file_cleaner_log = log.get_logger(__name__)
+
+# Present Data Dump Path
+data_path = '/home/apigban/PycharmProjects/SMP_scraper/Downloader/dump/'
+
+# Deletion Process for files that DIDN'T PASS the utf-8 encoding check
+invalid_file_log = '/home/apigban/PycharmProjects/SMP_scraper/Parser/log/invalid.txt'
 files_for_deletion = get_invalid_file()
-
 delete_invalid_file(files_for_deletion)
+
+# Pre-process for files that PASSED the utf-8 encoding check
+valid_file_log = '/home/apigban/PycharmProjects/SMP_scraper/Parser/log/valid.txt'
+files_for_processsing = get_Valid_file()
+
+string_pattern = "\A\^FINANCIAL"
+
+preproc_valid_files(files_for_processsing)
