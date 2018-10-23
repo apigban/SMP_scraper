42e06a80ac118da1f553c81d93722f0129867c10
Alain Igban <apigban@gmail.com>
2018-10-23 20:39:38 +0800

    Duplicate checking feature for db_commit_stock() function added.

--- Parser/db_func.py
+++ Parser/db_func.py
@@ -9,6 +9,7 @@ import Log.log as log
 from Parser import models
 from Parser.db_base import Base
 from Parser.db_config import *
+from .models import Stock
 
 db_logger = log.get_logger(__name__)
 
@@ -19,6 +20,7 @@ engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{datab
 Base.metadata.create_all(engine, checkfirst=True)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
 
 def db_commit_sector(row):
     """
@@ -54,6 +56,8 @@ def db_commit_stock(row):
     :param row:
     :return:
     """
+    duplicate_stock = None
+
     for item in row:
         symbol = row[0]
         trade_date = dt.strftime(dt.strptime(row[1], date_format), date_format)
@@ -62,12 +66,26 @@ def db_commit_stock(row):
         price_low = row[4]
         price_close = row[5]
         volume = row[6]
-        stock_id = symbol + trade_date
+        stock_id = symbol + trade_date.replace('-', '')
 
-    row = models.Stock(stock_id, symbol, trade_date, price_open, price_high, price_low, price_close, volume)
+    db_entry = Stock(stock_id, symbol, trade_date, price_open, price_high, price_low, price_close, volume)
 
-    session.add(row)
-    session.commit()
-    db_logger.info(f'Commit Done. Stock: {row.symbol}    Trade date:  {row.trade_date} ')
-    session.close()
-    return row
+    ### IF EXISTS, Ignore
+    # first get the object from the database if it exists
+    # IF stock_ id is None, then Create a new record and log
+    # otherwise, just log duplicates
+
+    if session.query(Stock).filter(Stock.stock_id == stock_id).first() is None:
+        unique_db_entry = Stock(stock_id, symbol, trade_date, price_open, price_high, price_low, price_close, volume)
+
+        session.add(unique_db_entry)
+        session.commit()
+        db_logger.info(f'Commit Done. Stock: {unique_db_entry.symbol}    Trade date:  {unique_db_entry.trade_date} ')
+        session.close()
+        return unique_db_entry
+
+    else:
+        duplicate_stock = 'duplicate'
+        db_logger.info(f'Duplicate Found: {db_entry.symbol}    Trade date:  {db_entry.trade_date}.  NO COMMIT')
+
+        return duplicate_stock
